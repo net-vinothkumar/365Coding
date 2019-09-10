154e6c8de851d6c0e2627f93bf8a57fb613ce2a5
vinothkumar <net.vinothkumar@gmail.com>
2019-09-10 19:35:46 +0200

    add a new test case for invalid age

--- src/test/kotlin/com/DaysProgramming/ILoveProgramming/swimming/SwimmingTicketTest.kt
+++ src/test/kotlin/com/DaysProgramming/ILoveProgramming/swimming/SwimmingTicketTest.kt
@@ -8,7 +8,13 @@ class SwimmingTicketTest {
     private val swimmingTicket = SwimmingTicket()
 
     @Test
-    fun `should return the length of the given sentence`() {
+    fun `should return the tickets price for the given invalid ages`() {
+        val ages = listOf(-1, -1, -1, -1, -1)
+        assertEquals(0, swimmingTicket.buyTickets(ages))
+    }
+
+    @Test
+    fun `should return the tickets price for the given ages`() {
         val ages = listOf(12, 19, 6, 46, 71)
         assertEquals(25, swimmingTicket.buyTickets(ages))
     }
