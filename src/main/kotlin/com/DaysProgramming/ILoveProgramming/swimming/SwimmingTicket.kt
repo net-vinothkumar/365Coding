1f5978b363933acda5e43708477d0067e4e73174
vinothkumar <net.vinothkumar@gmail.com>
2019-09-12 20:16:44 +0200

    fix the test cases

--- src/main/kotlin/com/DaysProgramming/ILoveProgramming/swimming/SwimmingTicket.kt
+++ src/main/kotlin/com/DaysProgramming/ILoveProgramming/swimming/SwimmingTicket.kt
@@ -11,27 +11,32 @@ class SwimmingTicket {
      * group of 5 students -> 1 student offer  -> student age between 16 -19
      */
     fun buyTickets(ages: List<Int>): Long {
-        val students = getStudentList(ages)
+        if (isValidAges(ages)) {
+            val students = getStudentList(ages)
 
-        if (ages.size == students.size) {
-            return 10L * (ages.size - 1)
+            if (ages.size == students.size) {
+                return 10L * (ages.size - 1)
+            }
+
+            return ages
+                    .stream()
+                    .map { age -> getPrice(age) }
+                    .mapToLong { value -> value }
+                    .sum()
         }
-
-        return ages
-                .stream()
-                .map { age -> getPrice(age) }
-                .mapToLong { value -> value }
-                .sum()
+        return 0
     }
 
-    private fun getStudentList(ages: List<Int>): MutableList<Int> {
+    private fun isValidAges(ages: List<Int>) = ages.stream().anyMatch { age -> age > 0 }
+
+    private fun getStudentList(ages: List<Int>): List<Int> {
         return ages
                 .stream()
-                .filter { age -> isStudentAge(age) }
+                .filter{ age -> isStudentAge(age) }
                 .collect(Collectors.toList())
     }
 
-    private fun isStudentAge(age: Int) = (age >= 16 || age <= 19)
+    private fun isStudentAge(age: Int) = (age in 16..19)
 
     private fun getPrice(age: Int): Long {
         return when {
