2a30b7aabd3cf0e3ddd6fe49f4aea1d6632a7cbd
vinothkumar <net.vinothkumar@gmail.com>
2019-09-23 23:01:06 +0200

    refactor

--- src/test/kotlin/com/DaysProgramming/ILoveProgramming/KinderGardenTest.kt
+++ src/test/kotlin/com/DaysProgramming/ILoveProgramming/KinderGardenTest.kt
@@ -9,6 +9,7 @@ class KinderGardenTest {
     fun `should add the kids name to the class`() {
         val kinderGarden = KinderGarden()
         kinderGarden.add("John")
+
         assertEquals(kinderGarden.getKids().get(0), "John")
     }
 }
