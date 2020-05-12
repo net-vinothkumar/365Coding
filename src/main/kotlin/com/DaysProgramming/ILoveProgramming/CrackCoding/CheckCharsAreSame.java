af1019735c334665a2f974fecc90f1c9394e1c95
vinothkumar <net.vinothkumar@gmail.com>
2020-05-12 15:41:45 +0200

    check characters are same

---
+++ src/main/kotlin/com/DaysProgramming/ILoveProgramming/CrackCoding/CheckCharsAreSame.java
@@ -0,0 +1,54 @@
+package com.DaysProgramming.ILoveProgramming.CrackCoding;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class CheckCharsAreSame {
+    public static void main(String[] args) {
+        solutionOne();
+        solutionTwo();
+    }
+
+    private static void solutionTwo() {
+        String input = "xxoo";
+        int xCount = (int) input.chars().mapToObj(c -> (char) c).filter(a -> Character.toLowerCase(a) == 'x').count();
+        int oCount = (int) input.chars().mapToObj(c -> (char) c).filter(a -> Character.toLowerCase(a) == 'o').count();
+
+        if (xCount == oCount) {
+            System.out.println("characters are same");
+        } else {
+            System.out.println("characters are NOT same");
+        }
+
+    }
+
+    private static void solutionOne() {
+        String input = "xxoo";
+        char[] characters = input.toCharArray();
+        Boolean isCharactersEqual = false;
+        Map<Character, Integer> characterMap = new HashMap<>();
+        for (char character : characters) {
+            if (characterMap.containsKey(character)) {
+                characterMap.put(character, characterMap.get(character) + 1);
+            } else {
+                characterMap.put(character, 1);
+            }
+        }
+
+        Integer previousValue = 0;
+        Set<Map.Entry<Character, Integer>> entrySet = characterMap.entrySet();
+        for (Map.Entry<Character, Integer> entry : entrySet) {
+            if (previousValue != 0 && !previousValue.equals(entry.getValue())) {
+                System.out.println("characters are NOT same");
+                return;
+            }
+            previousValue = entry.getValue();
+            isCharactersEqual = true;
+        }
+
+        if (isCharactersEqual) {
+            System.out.println("characters are same");
+        }
+    }
+}
--- src/main/kotlin/com/DaysProgramming/ILoveProgramming/CrackCoding/IsSquare.java
+++ src/main/kotlin/com/DaysProgramming/ILoveProgramming/CrackCoding/IsSquare.java
@@ -1,11 +1,24 @@
 package com.DaysProgramming.ILoveProgramming.CrackCoding;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class IsSquare {
     public static void main(String[] args) {
         System.out.println("3 * 3 = " + isSquare(9));
         System.out.println("4 * 4 = " + isSquare(16));
         System.out.println("5 * 5 = " + isSquare(25));
         System.out.println("4 * 5 = " + isSquare(20));
+        String song = "WUBWUBABCWUB";
+
+        String[] tokens = song.split("WUB");
+        StringBuilder result = new StringBuilder();
+        for (String token : tokens) {
+            if (!token.equals("WUB")) {
+                result.append(token).append(" ");
+            }
+        }
+        System.out.println(Stream.of(song.split("WUB")).filter(s -> ! s.isEmpty()).collect(Collectors.joining(" ")));
     }
 
     private static Boolean isSquare(int number) {
